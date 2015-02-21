da74e35583596700404cea3d16211d04c1aabb4c
maneckshaw <maneckshawb@gmail.com>
2015-02-22 00:06:22 +0530

    Update cachematrix.R
    make sets a matrix, obt obtains a matrix, dosolve sets the solved matrix in cache and obtsolve gets the solved matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment2 cachematrix.R
+## Creates a special matrix object that can cache its inverse
 
-## Write a short comment describing this function
+## This function creates a list of functions having setting matrix  and caching inverse of set matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+            im <- NULL
+            make <- function(y) {
+                 x <<- y
+                 im <- NULL
+                }
+            obt <- function() x
+            dosolve <- function(solve) im <<- solve
+            obtsolve <- function() im
+            list(make=make, obt=obt, dosolve=dosolve, obtsolve=obtsolve)
 
 }
 
 
-## Write a short comment describing this function
+
+## returns the inverse of a matrix created my makeCacheMatrix and if already found ...
+## ... it returns from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+            im <- x$obtsolve()
+            if(!is.null(im)){
+                   message("Getting cached data")
+                   return(im)
+            }
+           mat <- x$obt()
+           im <- solve(mat)
+           x$dosolve(im)
+           im
+           
 }
